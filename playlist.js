e67380330f129781b3d6967bce1fac290f1686fb
Devin Stone <devstone11@gmail.com>
2016-06-06 16:14:58 -0600

    Add album button functionality

--- playlist.js
+++ playlist.js
@@ -7,19 +7,30 @@ xmlObj.onreadystatechange = function() {
       var imgArr = [];
       for (var i = 0; i < 3; i++) {
         var randArt = myAlbums.splice(Math.floor(Math.random()*myAlbums.length),1)[0].cover_art;
-        if (randArt === "the_division_bell.jpg") {
-          imgArr.push("images/division_bell.jpg")
-        }else{
-          imgArr.push("images/"+randArt)
-        };
+        imgArr.push("images/"+randArt)
       }
       for (var i = 0; i < albumCovers.length; i++) {
         albumCovers[i].style.backgroundImage = "url("+imgArr[i]+")";
         albumCovers[i].style.backgroundSize = "100%";
       }
-      console.log(imgArr);
     }
 };
 
 xmlObj.open("GET", "https://lit-fortress-6467.herokuapp.com/object");
 xmlObj.send();
+
+$.get("https://lit-fortress-6467.herokuapp.com/object", function(data) {
+  data.results.forEach(function(thing) {
+    var div = $("<div>");
+    div.attr({class: "cover", id: thing.title})
+    div.css({"background-image": "url(images/" + thing.cover_art + ")", "background-size": "100%"});
+    $('#albums').append(div);
+  })
+  $('.cover').on("click", function(event) {
+    var album = $(this).attr("id")
+    var artist = data.results.reduce(function(prev,curr) {
+      return (curr.title === album) ? curr.artist : prev;
+    }, 0);
+    $('#playlist').append(`<div>${artist}: ${album}</div>`);
+  })
+})
