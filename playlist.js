684c890adaf20ff7cd3d2b40e051b7b7447bc801
Devin Stone <devstone11@gmail.com>
2016-06-07 14:28:22 -0600

    Add Spotify Album-picker

--- playlist.js
+++ playlist.js
@@ -1,15 +1,16 @@
-//Splash Page
+//Splash Page - raw javascript
 
 var xmlObj = new XMLHttpRequest();
 var albumCovers = document.getElementsByClassName("album");
 
 xmlObj.onreadystatechange = function() {
     if (xmlObj.readyState === 4 && xmlObj.status === 200) {
-      var myAlbums = JSON.parse(xmlObj.responseText).results;
+      var myAlbums = JSON.parse(xmlObj.responseText).albums;
+      console.log(myAlbums);
       var imgArr = [];
       for (var i = 0; i < 3; i++) {
-        var randArt = myAlbums.splice(Math.floor(Math.random()*myAlbums.length),1)[0].cover_art;
-        imgArr.push("images/"+randArt)
+        var randArt = myAlbums.splice(Math.floor(Math.random()*myAlbums.length),1)[0].images[1].url;
+        imgArr.push(randArt)
       }
       for (var i = 0; i < albumCovers.length; i++) {
         albumCovers[i].style.backgroundImage = "url("+imgArr[i]+")";
@@ -17,34 +18,43 @@ xmlObj.onreadystatechange = function() {
       }
     }
 };
-xmlObj.open("GET", "https://lit-fortress-6467.herokuapp.com/object");
+xmlObj.open("GET", "https://api.spotify.com/v1/albums/?ids=3NW7YlpaDa97jm259PIVCZ,0eQlvrTmnyi2HCzTlu1K9h,6TnJIiTtpSN4nQ64baw98y,1pQORO53AVvYinNFHJV2nh,2g0pjNFOGoKLlTP3GYouQA,6t3rvzvhC8lYvUwQLGH2BS,52Jy38DS81t5WdmsAw7Pbp,5814CZODdBLAbR9o3Vmw8S,7DjHF5MrTAamJRA2ffGq5P,2iI3eaYut9mBQ4jdBxnjDR,6C3nJC4oSZ8suGCi7Dde0i,6UPjOxqBNCDccXuEV5BNBf,1yIirSh11NsvcAUGATuSom,0KLIgHdbiAlmZXf7bmB7hI,6WP7K6Mn034qAGkHZc2892,7zyMzLKVWwksHhf5UvOwjf,2Aj14TcYeyGwpVhUMYxpQI,0LscekRzhfMQXuyTlJOTmZ,43ZhkQC08zbE7PDzMwhLpI,6zRtKtjdH9Vmx0KsgMK0hO");
 xmlObj.send();
 
-//Playlist Page
+//Playlist Page - jQuery
 
-$.get("https://lit-fortress-6467.herokuapp.com/object", function(data) {
-  data.results.forEach(function(thing) {
+$.get("https://api.spotify.com/v1/albums/?ids=3NW7YlpaDa97jm259PIVCZ,0eQlvrTmnyi2HCzTlu1K9h,6TnJIiTtpSN4nQ64baw98y,1pQORO53AVvYinNFHJV2nh,2g0pjNFOGoKLlTP3GYouQA,6t3rvzvhC8lYvUwQLGH2BS,52Jy38DS81t5WdmsAw7Pbp,5814CZODdBLAbR9o3Vmw8S,7DjHF5MrTAamJRA2ffGq5P,2iI3eaYut9mBQ4jdBxnjDR,6C3nJC4oSZ8suGCi7Dde0i,6UPjOxqBNCDccXuEV5BNBf,1yIirSh11NsvcAUGATuSom,0KLIgHdbiAlmZXf7bmB7hI,6WP7K6Mn034qAGkHZc2892,7zyMzLKVWwksHhf5UvOwjf,2Aj14TcYeyGwpVhUMYxpQI,0LscekRzhfMQXuyTlJOTmZ,43ZhkQC08zbE7PDzMwhLpI,6zRtKtjdH9Vmx0KsgMK0hO", function(data) {
+  data.albums.forEach(function(thing) {
     var div = $("<div>");
-    div.attr({class: "cover", id: thing.title})
-    div.css({"background-image": "url(images/" + thing.cover_art + ")", "background-size": "100%"});
-    $('#albums').append(div);
+    div.attr({class: "cover", id: thing.name})
+    div.css({"background-image": "url(" + thing.images[1].url + ")", "background-size": "100%"});
+    $('#insideAlbums').append(div);
   })
   $('.cover').on("click", function(event) {
     var album = $(this).attr("id")
-    var artist = data.results.reduce(function(prev,curr) {
-      return (curr.title === album) ? curr.artist : prev;
+    var artist = data.albums.reduce(function(prev,curr) {
+      return (curr.name === album) ? curr.artists[0].name : prev;
     }, 0);
-    $('#playlist').append(`<div>${artist}: ${album}</div>`);
+    $('#playlist').append("<div>"+artist + ": " + album + "</div>");
+    $(this).hide();
+    $('.post-results').hide();
   })
 })
 
 $('#clear').on("click", function() {
   $('#playlist').empty();
+  $('.cover').show();
+  $('.post-results').hide();
 })
+
+var postDiv = function(contents) {
+  return $("<div>" + contents + "</div>").css("margin", "15px 40px").addClass("post-results");
+};
 
 $('#submit').on("click", function() {
   $.post("https://lit-fortress-6467.herokuapp.com/post", function(data) {
-    console.log(data);
+    $('main').append(postDiv(data));
   })
   $('#playlist').empty();
+  $('.cover').show();
 })
